46c5df163921eb5bd7f84bdd5ff55fffeef20fac
Brent Shadel <brent.shadel@cerner.com>
2013-09-14 19:22:35 -0500

    Initial Commit

---
+++ GLUT_basics/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GLUT_basics
+//
+//  Created by Shadel,Brent on 9/14/13.
+//  Copyright (c) 2013 Shadel,Brent. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
